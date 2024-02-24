f2b80a3d69d8548891d906c3a61d3e2a13ca60eb
ElSimi0 <89326699+ElSimi0@users.noreply.github.com>
2024-02-24 01:36:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,73 @@
 # -*- coding: utf-8 -*-
 """
+
 Created on Sun Feb 18 21:22:37 2024
 
 @author: sameg
 """
 #!/usr/bin/env python3
 
-from tkinter import *
+#python C:\Users\sameg\Documents\AIprogWeb\JUEGOS\main.py
+
 import chistes
-
-def click():
-    label1 = Label(root, text=f"Hola {entrada.get()}!")
-    label1.pack()
-
-root = Tk()
-
-entrada = Entry(root)
-entrada.pack()
+import dragones
+import ahorcados
+import adivinaNumero
 
 
-boton = Button(root, text="CLICK", command=click, fg="black", bg="blue")
-boton.pack()
-
-root.mainloop()
-
+while True:
+    print("""
+          JUEGOS: Chistes, Dragones, Numero, Ahorcados
+          SALIR: x
+          """)
+    opcion = str(input("¿Que juego te interesa?: ")).lower()
+    
+    if opcion == "chiste" or opcion == "chistes":
+        print("\nEs un juego donde encontraras chistes\ndesde los mas familiares hasta unas\njoyitas escondidas")
+        while True:
+            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            if iniciar == "si" or iniciar == "y":
+                chistes.jugar()
+                break
+            elif iniciar == "no" or iniciar == "n":
+                break
+            else:
+                print("No te entendi ni vrga\n")            
+    elif opcion == "dragon" or opcion == "dragones":
+        print("\nRPG lineal sobre un calabozo, una gran\nhistoria y graficos excelentes (No tiene graficos)")
+        while True:
+            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            if iniciar == "si" or iniciar == "y":
+                dragones.jugar()
+                break
+            elif iniciar == "no" or iniciar == "n":
+                break
+            else:
+                print("No te entendi ni vrga\n")              
+    elif opcion == "ahorcados" or opcion == "ahorcado":
+        print("\nBasado en el manga original, este juego explora\nla cualidad humana bajo el ojo de la naturaleza")
+        while True:
+            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            if iniciar == "si" or iniciar == "y":
+                ahorcados.jugar()
+                break
+            elif iniciar == "no" or iniciar == "n":
+                break
+            else:
+                print("No te entendi ni vrga\n")
+    elif opcion == "numero" or opcion == "numeros":
+        print("numeros :)")
+        while True:
+            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            if iniciar == "si" or iniciar == "y":
+                adivinaNumero.jugar()
+                break
+            elif iniciar == "no" or iniciar == "n":
+                break
+            else:
+                print("No te entendi ni vrga\n")
+    elif opcion == "ninguno" or opcion == "x" or opcion == "salir":
+        break
+    else:
+        print("No entendi")
+                
