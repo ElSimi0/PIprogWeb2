f22827c3e0e132da66ad79983078f7f714bd343d
ElSimi0 <89326699+ElSimi0@users.noreply.github.com>
2024-02-24 09:48:05 -0600

    Actualizar ahorcados.py

--- ahorcados.py
+++ ahorcados.py
@@ -5,6 +5,14 @@ Created on Sun Feb 18 21:26:49 2024
 @author: sameg
 """
 import random
+
+azul = "\033[1;34m"
+amarillo = "\033[1;33m"
+blanco = "\033[0m"
+morado = "\033[1;35m"
+rojo = "\033[1;31m"
+verde = "\033[1;32m"
+
 IMÁGENES_AHORCADO = ['''
 
      +---+
@@ -73,7 +81,7 @@ def mostrarTablero(IMÁGENES_AHORCADO, letrasIncorrectas, letrasCorrectas, palab
       print(IMÁGENES_AHORCADO[len(letrasIncorrectas)])
       print()
 
-      print('Letras incorrectas:', end=' ')
+      print(rojo + 'Letras incorrectas:', end=' ')
       for letra in letrasIncorrectas:
           print(letra, end=' ')
       print()
@@ -85,32 +93,32 @@ def mostrarTablero(IMÁGENES_AHORCADO, letrasIncorrectas, letrasCorrectas, palab
               espaciosVacíos = espaciosVacíos[:i] + palabraSecreta[i] + espaciosVacíos[i+1:]
 
       for letra in espaciosVacíos: # mostrar la palabra secreta con espacios entre cada letra
-          print(letra, end=' ')
+          print(verde + letra, end=' ')
       print()
 
 def obtenerIntento(letrasProbadas):
       # Devuelve la letra ingresada por el jugador. Verifica que el jugador ha ingresado sólo una letra, y no otra cosa.
       while True:
-          print('Adivina una letra.')
+          print(azul + 'Adivina una letra.')
           intento = input()
           intento = intento.lower()
           if len(intento) != 1:
-              print('Por favor, introduce una letra.')
+              print(rojo + 'Por favor, introduce una letra.')
           elif intento in letrasProbadas:
-              print('Ya has probado esa letra. Elige otra.')
+              print(rojo + 'Ya has probado esa letra. Elige otra.')
           elif intento not in 'abcdefghijklmnñopqrstuvwxyz':
-              print('Por favor ingresa una LETRA.')
+              print(rojo + 'Por favor ingresa una LETRA.')
           else:
               return intento
 
 def jugarDeNuevo():
      # Esta función devuelve True si el jugador quiere volver a jugar, en caso contrario devuelve False.
-     print('¿Quieres jugar de nuevo? (sí o no)')
+     print(azul + '¿Quieres jugar de nuevo? (sí o no)')
      return input().lower().startswith('s')
 
 
 def jugar():
-    Juego = print('A H O R C A D O')  
+    Juego = print(blanco + 'A H O R C A D O')  
     letrasIncorrectas = ''
     letrasCorrectas = ''
     palabraSecreta = obtenerPalabraAlAzar(palabras)
@@ -131,7 +139,7 @@ def jugar():
                      encontradoTodasLasLetras = False
                      break
              if encontradoTodasLasLetras:
-                 print('¡Sí! ¡La palabra secreta es "' + palabraSecreta + '"! ¡Has ganado!')
+                 print(verde + '¡Sí! ¡La palabra secreta es "' + palabraSecreta + '"! ¡Has ganado!')
                  juegoTerminado = True
       else:
              letrasIncorrectas = letrasIncorrectas + intento
@@ -139,7 +147,7 @@ def jugar():
              # Comprobar si el jugador ha agotado sus intentos y ha perdido.
              if len(letrasIncorrectas) == len(IMÁGENES_AHORCADO) - 1:
                  mostrarTablero(IMÁGENES_AHORCADO, letrasIncorrectas, letrasCorrectas, palabraSecreta)
-                 print('¡Te has quedado sin intentos!\nDespués de ' + str(len(letrasIncorrectas)) + ' intentos fallidos y ' + str(len(letrasCorrectas)) + ' aciertos, la palabra era "' + palabraSecreta + '"')
+                 print(amarillo + '¡Te has quedado sin intentos!\nDespués de ' + blanco+ str(len(letrasIncorrectas)) + ' intentos fallidos y ' + str(len(letrasCorrectas)) + ' aciertos, la palabra era "' + palabraSecreta + '"')
                  juegoTerminado = True
     
          # Preguntar al jugador si quiere volver a jugar (pero sólo si el juego ha terminado).
@@ -151,7 +159,6 @@ def jugar():
                  palabraSecreta = obtenerPalabraAlAzar(palabras)
              else:
                 break
-
 if __name__ == "__main__":
     jugar()
 else:
