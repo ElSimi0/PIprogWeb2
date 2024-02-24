3b730f8402f92114886bbd774ef488001910c53f
ElSimi0 <89326699+ElSimi0@users.noreply.github.com>
2024-02-24 09:58:49 -0600

    Actualizar main.py

--- main.py
+++ main.py
@@ -14,60 +14,66 @@ import dragones
 import ahorcados
 import adivinaNumero
 
+azul = "\033[1;34m"
+amarillo = "\033[1;33m"
+blanco = "\033[0m"
+morado = "\033[1;35m"
+rojo = "\033[1;31m"
+verde = "\033[1;32m"
 
 while True:
-    print("""
+    print(blanco + """
           JUEGOS: Chistes, Dragones, Numero, Ahorcados
           SALIR: x
           """)
-    opcion = str(input("¿Que juego te interesa?: ")).lower()
+    opcion = str(input(azul + "¿Que juego te interesa?: "+ blanco)).lower()
     
     if opcion == "chiste" or opcion == "chistes":
-        print("\nEs un juego donde encontraras chistes\ndesde los mas familiares hasta unas\njoyitas escondidas\n\n")
+        print(verde + "\nEs un juego donde encontraras chistes\ndesde los mas familiares hasta unas\njoyitas escondidas\n\n")
         while True:
-            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            iniciar = str(input(azul + "Desea Jugar este Juego (y/n) ?: " + blanco)).lower()
             if iniciar == "si" or iniciar == "y":
                 chistes.jugar()
                 break
             elif iniciar == "no" or iniciar == "n":
                 break
             else:
-                print("No te entendi ni vrga\n")            
+                print(rojo + "No te entendi ni vrga\n")            
     elif opcion == "dragon" or opcion == "dragones":
-        print("\nRPG lineal sobre un calabozo, una gran\nhistoria y graficos excelentes (No tiene graficos)\n")
+        print(morado + "\nRPG lineal sobre un calabozo, una gran\nhistoria y graficos excelentes (No tiene graficos)\n")
         while True:
-            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            iniciar = str(input(azul + "Desea Jugar este Juego (y/n) ?: "+ blanco)).lower()
             if iniciar == "si" or iniciar == "y":
                 dragones.jugar()
                 break
             elif iniciar == "no" or iniciar == "n":
                 break
             else:
-                print("No te entendi ni vrga\n")              
+                print(rojo + "No te entendi ni vrga\n")              
     elif opcion == "ahorcados" or opcion == "ahorcado":
-        print("\nBasado en el manga original, este juego explora\nla cualidad humana bajo el ojo de la naturaleza\n")
+        print(verde + "\nBasado en el manga original, este juego explora\nla cualidad humana bajo el ojo de la naturaleza\n")
         while True:
-            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            iniciar = str(input(azul + "Desea Jugar este Juego (y/n) ?: " + blanco)).lower()
             if iniciar == "si" or iniciar == "y":
                 ahorcados.jugar()
                 break
             elif iniciar == "no" or iniciar == "n":
                 break
             else:
-                print("No te entendi ni vrga\n")
+                print(rojo + "No te entendi ni vrga\n")
     elif opcion == "numero" or opcion == "numeros":
-        print("\nnumeros :)\n")
+        print(morado + "\nnumeros :)\n")
         while True:
-            iniciar = str(input("Desea Jugar este Juego (y/n) ?: ")).lower()
+            iniciar = str(input(azul + "Desea Jugar este Juego (y/n) ?: ")).lower()
             if iniciar == "si" or iniciar == "y":
                 adivinaNumero.jugar()
                 break
             elif iniciar == "no" or iniciar == "n":
                 break
             else:
-                print("No te entendi ni vrga\n")
+                print(rojo + "No te entendi ni vrga\n")
     elif opcion == "ninguno" or opcion == "x" or opcion == "salir":
         break
     else:
-        print("No entendi")
+        print(rojo + "No entendi")
                 
